6d91778053f6634afce8693259a0c089ba187888
SzymonOzog <szymon.ozog@aleph-alpha.com>
2025-03-12 10:55:09 +0000

    add mtp to gguf model types

--- vllm/model_executor/model_loader/loader.py
+++ vllm/model_executor/model_loader/loader.py
@@ -1277,7 +1277,7 @@ class GGUFModelLoader(BaseModelLoader):
         # hack: ggufs have a different name than transformers
         if model_type == "cohere":
             model_type = "command-r"
-        if model_type in ("deepseek_v3", "deepseek_v2"):
+        if model_type in ("deepseek_v3", "deepseek_v2", "deepseek_mtp"):
             model_type = "deepseek2"
             # GGUF layer map assumes that we will have a merged expert weights
             # so we need to map them manually
